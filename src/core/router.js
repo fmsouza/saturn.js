f17cfbc6a9780ff925f4f49375d3a17f69cb8df4
Fred Souza <fredericoamsouza@gmail.com>
2015-12-02 16:58:01 -0200

    Allowing the needed HTTP Methods

--- src/core/router.js
+++ src/core/router.js
@@ -23,6 +23,7 @@ class Router {
         this.driver.use((req, res, next) => {
             res.header('Access-Control-Allow-Origin', '*');
             res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+            res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
             logger.info(`Serving route ${req.url} (${req.method})`);
             next();
         });
