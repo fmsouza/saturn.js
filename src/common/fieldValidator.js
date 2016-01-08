b02bb05a419173fb62b91cc45a5b1ee4d4dfe049
Fred Souza <fredericoamsouza@gmail.com>
2016-01-08 11:51:19 -0200

    Documenting

--- src/common/fieldValidator.js
+++ src/common/fieldValidator.js
@@ -1,16 +1,37 @@
 'use strict';
 
+/**
+ * FieldValidator class is responsible for validating the inputted data in the API requests
+ * before doing the communication with the database
+ * @class {FieldValidator} 
+ */
 class FieldValidator {
     
     constructor(rules) {
         this.rules = rules;
     }
     
+    /**
+     * Checks it the field content type meets the required type
+     * @param {string} type - Field type
+     * @param {string} field - Field name
+     * @param {Function} fn - Procedure to check the type
+     * @return {*}
+     * @throw {Error}
+     */
     checkType(type, field, fn) {
         try { return fn(); }
         catch(e) { throw new Error(`The type of the field '${field}' content must be '${type}'.`); }
     }
     
+    /**
+     * Tries to parse the field value to it's expected type
+     * @param {string} type - Field type
+     * @param {*} value - Field content
+     * @param {string} field - Field name
+     * @return {*}
+     * @throw {Error}
+     */
     parseType(type, value, field) {
         if(type==='string') return this.checkType(type, field, () => {
             return value.toString();
@@ -27,6 +48,12 @@ class FieldValidator {
         });
     }
     
+    /**
+     * Validates the input data
+     * @param {Object} data - Input data
+     * @return {Object}
+     * @throw {Error}
+     */
     validate(data) {
         let output = {};
         for(let rule of Object.keys(this.rules)) {
